8f2df3d3460ea5a8f13eb4df58edf48e037cb7c3
Tec <Tec@SKAB1-PC1>
2023-08-08 12:40:18 +0200

    Add project files.

---
+++ bilopg/Program.cs
@@ -0,0 +1,127 @@
+﻿class Car
+{
+    public string lave { get; set; }
+    public string Model { get; set; }
+    public string Nummerplade { get; set; }
+    public string Mærke { get; set; }
+    public string Størrelse { get; set; }
+    public string Dato { get; set; }
+}
+
+class Customer
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string kontraktinfo { get; set; }
+    public List<Car> bil { get; } = new List<Car>();
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Customer> customers = new List<Customer>();
+
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("Registrerede kunder:");
+            for (int i = 0; i < customers.Count; i++)
+            {
+                Console.WriteLine("{0}. {1}", i, customers[i].FirstName);
+            }
+
+            Console.WriteLine("\nVelkommen til kundeadministrationssystemet!");
+            Console.WriteLine("1. Registrer ny kunde");
+            Console.WriteLine("2. Vis kunde kontraktinfo");
+            Console.WriteLine("3. Afslut");
+
+            Console.Write("Vælg en handling ( 1 / 2 / 3 ): ");
+            string vælge = Console.ReadLine();
+
+            switch (vælge)
+            {
+                case "1":
+                    Console.Clear();
+
+
+                    Customer newCustomer = new Customer();
+                    Car newCar = new Car();
+
+                    Console.Write("Indtast kundens FirstName: ");
+                    newCustomer.FirstName = Console.ReadLine();
+
+                    Console.Write("Indtast kundens LastName: ");
+                    newCustomer.LastName = Console.ReadLine();
+
+                    Console.Write("Indtast kundens kontaktoplysninger: ");
+                    newCustomer.kontraktinfo = Console.ReadLine();
+
+                    Console.Write("Indtast kundens Nummerplade: ");
+                    newCar.Nummerplade = Console.ReadLine();
+
+                    Console.Write("Indtast kundens Mærke: ");
+                    newCar.Mærke = Console.ReadLine();
+
+                    Console.Write("Indtast kundens Model: ");
+                    newCar.Model = Console.ReadLine();
+
+                    Console.Write("Indtast kundens Moterstørrelse: ");
+                    newCar.Størrelse = Console.ReadLine();
+
+                    Console.Write("Indtast kundens Dato: ");
+                    newCar.Dato = Console.ReadLine();
+
+
+                    customers.Add(newCustomer);
+
+                    Console.Clear();
+
+                    Console.WriteLine("Kunde registreret!");
+
+
+
+
+                    
+                    break;
+
+
+
+
+                case "2":
+                    Console.Write("Indtast kundens nummer (0 - {0}): ", customers.Count - 1);
+                    int customerNumber;
+                    if (int.TryParse(Console.ReadLine(), out customerNumber) && customerNumber >= 0 && customerNumber < customers.Count)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Kontaktinfo: ");
+                        Customer customer = customers[customerNumber];
+                        Console.WriteLine("Navn: {0}\nKontaktinfo: {1}", customer.FirstName, customer.kontraktinfo);
+                        //Console.WriteLine("Registrerede biler: ");
+                        /*foreach (Car cars in ShowCar.bil)
+                        {
+                            Console.WriteLine("Mærke: {0}, Model: {1}", cars.lave, cars.Model);
+                        }*/
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ugyldigt kundenummer.");
+                    }
+                    break;
+
+
+
+
+                case "3":
+                    running = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Ugyldigt valg. Vælg en gyldig handling.");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
